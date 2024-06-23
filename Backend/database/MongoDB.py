4114170874fecdb92fe7a36e8949dea238d94d38
= <pinildissanayaka@gmail.com>
2024-06-23 22:57:29 +0530

    add singleton for mongo client

--- Backend/database/MongoDB.py
+++ Backend/database/MongoDB.py
@@ -1,16 +1,23 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from threading import Lock
 
 load_dotenv(dotenv_path='.env')
 
 class MongoDB(object):
     def __init__(self) -> None:
         self._CONNECTION_STRING=os.getenv(key='CONNECTION_STRING')
+        self._client=None
+        self.lock=Lock()
         
     def connectDB(self, collectionName:str):
-        client=MongoClient(self._CONNECTION_STRING)
-        collection=client[collectionName]
+        if self._client is None:
+            with self.lock:
+                if self._client is None:
+                    client=MongoClient(self._CONNECTION_STRING)
+                    collection=client[collectionName]
+                    
         return  client, collection
 
         
