b7e516b00a44f2d8325ef8d1e94403b619808605
= <pinildissanayaka@gmail.com>
2024-06-24 18:36:48 +0530

    add user end point done

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from database.user import User
 
 app=FastAPI()
+user=User()
 
 
 @app.get('/')
@@ -10,3 +12,19 @@ async def root():
 @app.get('/item/{item_id}')
 async def root_item(item_id:int):
     return {"massage" :item_id}
+
+
+@app.post('/add_user')
+async def addUser(request :Request):
+    try:
+        data=await request.json()
+        
+        if 'userName' not in data or 'password' not in data:
+            raise HTTPException(status_code=422,
+                                detail="Incomplete data provided")
+        else:
+            user.addUser(data=data)
+    except HTTPException as e:
+        raise e
+    
+            
